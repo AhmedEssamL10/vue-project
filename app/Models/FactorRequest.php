2dd0db832be6206c5816c7a3e4184f07467023f2
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2025-06-04 03:43:05 +0300

    update

---
+++ app/Models/FactorRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FactorRequest extends Model
+{
+    protected $fillable = [
+        'count',
+        'date_from',
+        'date_to',
+        'client_name',
+        'client_email',
+        'client_phone',
+    ];
+}
