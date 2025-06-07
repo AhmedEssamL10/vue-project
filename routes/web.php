daf7229be47da3d3883f7d7aad2a678e4114e2b7
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2025-06-07 05:53:45 +0300

    auth users

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\API\RequestController;
 
 // Blade homepage
@@ -28,3 +29,15 @@ Route::get('/app/{any?}', function () {
 // Route::get('/dashboard/{any?}', function () {
 //     return view('vue-request'); // Vue entrypoint Blade file    
 // })->where('any', '.*');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
