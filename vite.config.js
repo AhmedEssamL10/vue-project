fb7d52d918fbf666c94e98df9a706aa91cf4c875
Salem Tarek <salemtarek224@gmail.com>
2025-06-13 05:27:18 +0300

    Fixing build

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import vue from '@vitejs/plugin-vue'
-import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/main.js"],
-            refresh: true,
-        }),
-        vue()
-    ],
-    resolve: {
-        alias: {
-          vue: 'vue/dist/vue.esm-bundler.js',
-          '@': path.resolve(__dirname, 'resources/js'),
-        },
-      },
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/js/main.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    rollupOptions: {
+      input: 'resources/js/main.js',
+    },
+  },
 });
