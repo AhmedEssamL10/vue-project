9c0f5f5148e0b8f6b40429aa9f657c9bf5b9da64
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2025-06-13 03:51:50 +0300

    update\

--- routes/auth.php
+++ routes/auth.php
@@ -9,18 +9,19 @@ use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     // Route::get('register', [RegisteredUserController::class, 'create'])
     //     ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store'])->name('register');
+    Route::post('register', [AuthController::class, 'register'])->name('register');
 
     // Route::get('login', [AuthenticatedSessionController::class, 'create'])
     //     ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
+    Route::post('login', [AuthController::class, 'login'])->name('login');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
