edf06c79a03c9dbb708a8fa1884ab0ede1340545
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2025-06-14 04:01:25 +0300

    profile

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,9 @@ Route::get('/dashboard/{any?}', function () {
 // })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile', function () {
+        return view('profile.edit'); // Blade-only view for profile editing
+    })->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
