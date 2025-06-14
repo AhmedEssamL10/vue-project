b9b095a2e91318e183f7462424f9bfe79b2edd93
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2025-06-15 01:28:11 +0300

    add phone and cliend type

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,6 +34,12 @@
                                <input type="email" id="email" name="email" placeholder=" {{ __('Email') }}"
                                    class="w-full px-4 py-3 rounded-lg bg-white transition duration-200 outline-none text-[#1b1718]" />
                            </div>
+                           <div>
+                               <label for="phone" class="block text-sm font-medium text-gray-700 mb-1">
+                                   {{ __('Phone') }}</label>
+                               <input type="tel" id="phone" name="phone" placeholder=" {{ __('Phone') }}"
+                                   class="w-full px-4 py-3 rounded-lg bg-white transition duration-200 outline-none text-[#1b1718]" />
+                           </div>
 
                            <!-- Password Input -->
                            <div>
