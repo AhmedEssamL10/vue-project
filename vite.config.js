0583f31c0f4ca1d8578c6e64935633c42c9e1f8a
Salem Tarek <salemtarek224@gmail.com>
2025-06-23 22:44:53 +0300

    Adding applicant message to register and germanize dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,11 @@ export default defineConfig({
       refresh: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'public/assets')
+    }
+  },
   build: {
     manifest: true,
     outDir: 'public/build',
