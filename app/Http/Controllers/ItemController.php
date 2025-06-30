0c5783f661f05fa37677c8ae6a257673d930423f
Ahmed Essam <201927243@std.sci.cu.edu.eg>
2025-07-01 01:24:37 +0300

    update

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -11,7 +11,7 @@ class ItemController extends Controller
 {
     public function index()
     {
-        return response()->json(["data" => Item::all()]);
+        return response()->json(["data" => Item::whre('status', 'active')->get()]);
     }
 
     public function store(Request $request)
